89628f6e0b3260b8bee68f8f70932dc78310dd0d
Prince Vaviya <1princevaviya@gmail.com>
2026-08-25 12:08:18 +0530

    git project uploaded

--- 10_class.dart
+++ 10_class.dart
@@ -1,1 +1,23 @@
-void main() {}
+class Book {
+  String title = "";
+  String author = "";
+  String genre = "";
+
+  void displayBook() {
+    print("Book Title: $title");
+    print("Book Author: $author");
+    print("Book Genre: $genre");
+  }
+}
+
+void task1() {
+  Book book = Book();
+
+  book.title = "The Three-Body Problem";
+  book.author = "Cixin Liu";
+  book.genre = "Sci-Fi";
+}
+
+void main() {
+  task1();
+}
--- 11_constructor.dart
+++ 11_constructor.dart
@@ -1,1 +1,26 @@
-void main() {}
+class Book {
+  String title = "";
+  String author = "";
+  String genre = "";
+
+  Book(this.title, this.author, this.genre);
+
+  void displayBook() {
+    print("Book Title: $title");
+    print("Book Author: $author");
+    print("Book Genre: $genre");
+  }
+}
+
+void task2() {
+  Book book0 = Book("The Three-Body Problem", "Cixin Liu", "Sci-Fi");
+  Book book1 = Book("Blindsight", "Peter Watts", "Sci-Fi");
+
+  book0.displayBook();
+  print("------------------");
+  book1.displayBook();
+}
+
+void main(){
+  task2();
+}
